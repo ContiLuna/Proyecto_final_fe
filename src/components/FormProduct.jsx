0d7408905c61256a3bb2b1664797b7ada2a633ed
sebazelarayan12 <sebazelarayan12@gmail.com>
2022-12-17 14:06:36 -0300

    estilos crear productos con errores

--- src/components/FormProduct.jsx
+++ src/components/FormProduct.jsx
@@ -23,67 +23,84 @@ const FormProducto = () => {
   return (
     <div className="form">
       <Form className="form-product" onSubmit={handleSubmit}>
-        <FormGroup className='form-container'>
-          <Label for="exampleEmail">Nombre</Label>
-          <Input
-            name="nombre"
-            placeholder="Agregue un nombre"
-            type="text"
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="exampleEmail">Estado</Label>
-          <Input
-            name="estado"
-            type="select"
-            onChange={handleChange}
-          >
-            <option>Seleccione un estado</option>
-            <option value="disponible">Disponible</option>
-            <option value="no disponible">No disponible</option>
-          </Input>
-        </FormGroup>
-        <FormGroup>
-          <Label for="examplePassword">Precio</Label>
-          <Input
-            name="precio"
-            placeholder="Ingrese precio"
-            type="number"
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="examplePassword">Detalle</Label>
-          <Input
-            name="detalle"
-            placeholder="Ingrese descripcion del menú"
-            type="text"
-            onChange={handleChange}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="examplePassword">Foto del menú</Label>
-          <Input
-            name="imagen"
-            type="file"
-            onChange={handleFiles}
-          />
-        </FormGroup>
-        <FormGroup>
-          <Label for="examplePassword">Categoria</Label>
-          <Input
-            name="categoria"
-            type="select"
-            onChange={handleChange}
-          >
-            <option>Seleccione una categoria</option>
-            <option value="marvel">Hamburguesas</option>
-            <option value="dc">Pizzas</option>
-          </Input>
-        </FormGroup>
+        <h2 className="form-title">Crear un producto nuevo</h2>
+        <div className="form-container">
+          <FormGroup className="form-group">
+            <Input
+              className="form-input"
+              name="nombre"
+              placeholder="Agregue un nombre"
+              type="text"
+              onChange={handleChange}
+            />
+            <Label className="form-label" for="nombre">
+              Nombre
+            </Label>
+            <span className="form-line"></span>
+          </FormGroup>
+          <FormGroup className="form-group">
+            <Input
+              className="form-input"
+              name="estado"
+              type="select"
+              onChange={handleChange}
+            >
+              <option>Seleccione un estado</option>
+              <option value="disponible">Disponible</option>
+              <option value="no disponible">No disponible</option>
+            </Input>
+          </FormGroup>
+          <FormGroup className="form-group">
+            <Input
+              className="form-input"
+              name="precio"
+              placeholder="Ingrese precio"
+              type="number"
+              onChange={handleChange}
+            />
+            <Label className="form-label" for="precio">
+              Precio
+            </Label>
+            <span className="form-line"></span>
+          </FormGroup>
+          <FormGroup className="form-group">
+            <Input
+              className="form-input"
+              name="detalle"
+              placeholder="Ingrese descripcion del menú"
+              type="text"
+              onChange={handleChange}
+            />
+            <Label className="form-label" for="detalle">
+              Detalle
+            </Label>
+            <span className="form-line"></span>
+          </FormGroup>
+          <FormGroup className="form-group">
+            <Input
+              className="form-input"
+              name="imagen"
+              type="file"
+              onChange={handleFiles}
+            />
+          </FormGroup>
+          <FormGroup className="form-group">
+            <Input
+              className="form-input"
+              name="categoria"
+              type="select"
+              onChange={handleChange}
+            >
+              <option>Seleccione una categoria</option>
+              <option value="hamburguesas">Hamburguesas</option>
+              <option value="pizzas">Pizzas</option>
+            </Input>
+          </FormGroup>
 
-        <Button>Submit</Button>
+          <Button className="form-submit" value="enviar">
+            Submit
+          </Button>
+        </div>
       </Form>
     </div>
   );
