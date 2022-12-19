539528d88e9006819ab496b453f9e072957597aa
NielzBrz <danielbrz1999@gmail.com>
2022-12-19 20:11:42 -0300

    botones redes - logo default - copyR - falta contacto

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,34 +6,30 @@ const Footer = () => {
         <div className='main-footer text-white bg-dark'>
             <div className="container">
                 <div className="row ">
-                    <div className="col-md-3 col-sm-3">
-                        <h3 className='text-center'>Algo</h3>
+                    <div className="col-md-4 col-sm-4">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvZki1EoCKV8aavs6hmPzqmnWUg-USH2qEiZSIhr3f6pa92z1DujPF10BHO8fXBI25zFY&usqp=CAU" 
+                        target='_blank' alt="Logo Team" width={'150px'} 
+                        className='d-flex justify-content-center mx-auto mt-2'/>
+                    </div>
+                    <div className="col-md-4 col-sm-4 mt-2">
+                        <h4 className='text-center'>Contacto</h4>
                         <ul className='list-unstyled'>
-                            <li className='text-center'>Alguna info</li>
-                            <li className='text-center'>Alguna info</li>
-                            <li className='text-center'>Alguna info</li>
+                            <li className='text-center'>Hotmail</li>
+                            <li className='text-center'>Num Cel</li>
                         </ul>
                     </div>
-                    <div className="col-md-6 col-sm-6">
-                        <h3 className='text-center'>Algo2</h3>
-                        <ul className='list-unstyled'>
-                            <li className='text-center'>Alguna info</li>
-                            <li className='text-center'>Alguna info</li>
-                            <li className='text-center'>Alguna info</li>
-                        </ul>
-                    </div>
-                    <div className="col-md-3 col-sm-3">
-                        <h3 className='text-center'>Algo3</h3>
-                        <ul className='list-unstyled'>
-                            <li className='text-center'>Alguna info</li>
-                            <li className='text-center'>Alguna info</li>
-                            <li className='text-center'>Alguna info</li>
+                    <div className="col-md-4 col-sm-4">
+                        <h4 className='text-center mt-2'>Seguinos en nuestras redes</h4>
+                        <ul className='list-unstyled d-flex justify-content-between m-4'>
+                            <a href="https://facebook.com" target='_blank' role="button" class="btn btn-primary text-center"><i class="bi bi-facebook"></i></a>
+                            <a href="https://instagram.com" target='_blank' role="button" class="btn btn-danger text-center"><i class="bi bi-instagram"></i></a>
+                            <a href="https://twitter.com" target='_blank' role="button" class="btn btn-info text-center"><i class="bi bi-twitter"></i></a>
                         </ul>
                     </div>
                 </div>
                 <div className="footer-bottom">
-                    <p className='text-center'>
-                        &copy;{new Date().getFullYear()} Creado por
+                    <p className='text-center mt-2'>
+                        Copyright &copy;{new Date().getFullYear()} Todos los derechos reservados
                     </p>
                 </div>
             </div>
