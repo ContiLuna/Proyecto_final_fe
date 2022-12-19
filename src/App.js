2617bbfef5041a09c51647381eb2793f87c4c714
clementecejas <cuchi_cejas@live.com>
2022-12-19 20:11:35 -0300

    agregando css y funciones

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Buscador from "./components/Buscador";
 
 function App() {
   return (
-   <Routes> 
-    <Route path='/' element={<h1>trabajen pues</h1>}/>
-    <Route path='/login' element={ <Login /> } />
-   </Routes>
+    <div className="App">
+      <Buscador />
+      <Routes>
+        <Route path="/" element={<h1>trabajen pues</h1>} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
   );
 }
 
