7215b3164b8bbbff34e0c7674b8e6b53a0864ee5
NielzBrz <danielbrz1999@gmail.com>
2022-12-20 15:23:43 -0300

    cambio de color y logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,40 +1,30 @@
 import React, { Component } from 'react'
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
+// import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import "./NavBar.css";
 
 
 function NavBar() {
     return (
-      <Navbar collapseOnSelect bg="dark" variant={"dark"} expand="md" sticky="top" >
+      <Navbar collapseOnSelect expand="sm" sticky="top" className='Navbcss'>
         <Container fluid>
-          <Navbar.Brand className='' href="#">Logo</Navbar.Brand>
+          <Navbar.Brand className='' href="#"><img 
+          src="https://i.ibb.co/x27ShPx/Ok-food-191919.png"
+          alt="Logo" width={'50px'}/></Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto text-center text-md-start" style={{ maxHeight: '100%' }}>
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Pedidos</Nav.Link>
-              <Nav.Link href="#action3">Nosotros</Nav.Link>
-              <Nav.Link href="#action4">Contacto</Nav.Link>
+              <Nav className="container gap-1 flex-column flex-sm-row justify-content-center justify-content-sm-end" 
+              style={{ maxHeight: '100%' }}>
+              <Nav.Link href="#action1" className='d-inline-flex justify-content-center'>Home</Nav.Link>
+              <Nav.Link href="#action2" className='d-inline-flex justify-content-center'>Pedidos</Nav.Link>
+              <Button type='button' 
+              className="btn btn-dark">Ingresar</Button>
+              <Button type='button'
+              className="btn btn-dark">Registro</Button>
             </Nav>
-            <Form className="d-flex mt-1">
-              <Form.Control
-                type="search"
-                placeholder="Buscar Producto"
-                className="me-1"
-                aria-label="Search"
-              />
-              <Button variant="outline-success"
-              className="me-1">Search</Button>
-            </Form>
-            <div className='mt-1 d-grid d-md-flex justify-content-center flex-md-row flex-sm-column' role="group">
-            <Button variant="outline-danger"
-            className="me-1 text-center">Login</Button>
-            <Button variant="outline-danger"
-            className="me-1 text-center">Register</Button>
-            </div>
           </Navbar.Collapse>
         </Container>
       </Navbar>
