ff326e10a705d53df6f723efcd7f8f0fe288f500
ContiLuna <lunaflorenciaconstanza@gmail.com>
2022-12-22 18:44:53 -0300

    se agrega url back

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,21 +2,21 @@ import { useState } from 'react';
 import { Header } from '../components/Header';
 import { ProductList } from '../components/ProductList';
 
-function App() {
+function Pedidos() {
 	const [allProducts, setAllProducts] = useState([]);
 	const [total, setTotal] = useState(0);
 	const [countProducts, setCountProducts] = useState(0);
 
 	return (
 		<>
-			<Header
+			{/* <Header
 				allProducts={allProducts}
 				setAllProducts={setAllProducts}
 				total={total}
 				setTotal={setTotal}
 				countProducts={countProducts}
 				setCountProducts={setCountProducts}
-			/>
+			/> */}
 			<ProductList
 				allProducts={allProducts}
 				setAllProducts={setAllProducts}
@@ -29,4 +29,4 @@ function App() {
 	);
 }
 
-export default App;
+export default Pedidos;
