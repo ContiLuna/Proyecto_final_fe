f37315fc6333b3f114c41127ec009127e09ddb21
ContiLuna <lunaflorenciaconstanza@gmail.com>
2022-12-21 22:25:40 -0300

    agregado useeffect en menu

--- src/pages/Admin/Menu.jsx
+++ src/pages/Admin/Menu.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import "./admin.css"
 import FormProducto from '../../components/FormProduct/FormProduct'
 import ModalReact, { ModalBodyReact, ModalHeaderReact } from '../../components/Modal/ModalReact'
 import TablaMenu from '../../components/Tablas/TablaMenu'
+import UserContext from '../../context/UserContext';
+import { getAllPedidos } from '../../context/UserActions';
 
 const Menu = () => {
     const [showModal, setShowModal] = useState(false)
+    const {state, dispatch} = useContext(UserContext);
+
+    useEffect(() => {
+        dispatch(getAllPedidos())
+    }, [])
+    
   return (
     <>
     <div style={{height: "calc(90vh - 60px)"}}>
