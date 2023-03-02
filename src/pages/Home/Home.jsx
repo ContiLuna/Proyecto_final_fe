bdbfec74554d2d46431260033323a6d7608168ee
Mauricio Teruel <mauricioteruel98@gmail.com>
2023-03-01 22:27:05 -0300

    Cambios en estilos de sugerencias y estilos de la web

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,9 @@ import Card from "../../components/Card/Card";
 import Carrusel from "../../components/Carrusel/Carrusel";
 import Categories from "../../components/Categories/CategoriesForm";
 import Productos from "../../components/Productos/Productos";
-// import Sugerencias from "../../components/Sugerencias/Sugerencias";
+import Sugerencias from "../../components/Sugerencias/Sugerencias";
 import UserContext from "../../context/UserContext";
+import Carousel from 'react-bootstrap/Carousel';
 // import { menus } from "../../dataUsuarios";
 import "./Home.css";
 
@@ -51,22 +52,49 @@ const Home = () => {
         <Carrusel />
       </div>
       <div className="home__producto">
-        <div className="home__producto__title">
-          <h1>Consulta nuestros Productos</h1>
-        </div>
-        <div className="home__cards">
-          {productos.map((producto) => (
-            <Productos key={producto.id} producto={producto} />
-          ))}
-        </div>
+
       </div>
       <div>
+        <div className="sugerencias1 my-5">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6 d-flex justify-content-center align-items-center">
+                <div className="texto-sugerencias px-4 py-5">
+                  <h1 className="mb-5">Nuestras sugerencias!</h1>
+                  <h4 className="mt-5">Ok food tiene las siguientes recomendaciones para el dia de hoy, son nuestros platos y productos sugeridos de hoy. Pedilos y Disfrutalos!! ;)</h4>
+                </div>
+              </div>
+              <div className="col-md-6 mt-5 mt-md-0">
+                <Carousel indicators={false}>
+                  {
+                    state?.productos
+                      //.filter((menu) => menu.sugerido === true)
+                      .map((menu) => (
+                        <Carousel.Item key={menu._id}>
+                          <div className="d-flex justify-content-center">
+                            <Sugerencias
+                              menuId={menu._id}
+                              key={menu._id}
+                              title={menu.nombre}
+                              description={menu.detalle}
+                              price={menu.precio}
+                              img={menu.imagen}
+                            />
+                          </div>
+                        </Carousel.Item>
+                      ))
+                  }
+                </Carousel>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="home__producto__title">
           <h1>Mira nuestros menus</h1>
         </div>
-        <Categories 
-        onSelectCategory={handleCategorySelect} 
-        onDeselectCategory={handleCategoryDeselect}/>
+        <Categories
+          onSelectCategory={handleCategorySelect}
+          onDeselectCategory={handleCategoryDeselect} />
         <div className="sugerencias">
           {state?.productos
             .filter((menu) => selectedCategory === null || menu.categoria === selectedCategory)
