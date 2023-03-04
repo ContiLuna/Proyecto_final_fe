458db2f8eb16057795fa77438be1345331e93d6a
clementecejas <cuchi_cejas@live.com>
2023-03-04 13:46:50 -0300

    centrando nav

--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 // import Form from 'react-bootstrap/Form';
@@ -19,7 +19,7 @@ function NavBar() {
   };
   return (
     <Navbar collapseOnSelect expand="sm" sticky="top" className="Navbcss">
-      <Container fluid>
+      <Container fluid className="container-nav">
         <Link to="/">
         <Navbar.Brand>
           <img
