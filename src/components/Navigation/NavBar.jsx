9a603854ad0de190166bdaf0505f7f67ff8f822d
clementecejas <cuchi_cejas@live.com>
2023-03-04 18:44:13 -0300

    borrando boton nosotros

--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -89,13 +89,6 @@ function NavBar() {
                 >
                   Registro
                 </Button>
-                <Button
-                  onClick={() => navigate("/nosotros")}
-                  type="button"
-                  className="btn btn-dark"
-                >
-                  Nosotros
-                </Button>
               </>
             )}
           </Nav>
